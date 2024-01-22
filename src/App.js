f42a4aa501786ea87f639b5c918d3653919a4631
Mack Octavian <maestrooctavian@gmail.com>
2024-01-22 14:05:05 +0300

    Created book list and displyed the list dynamically

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: "https://reactjs.org",
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID:0,
+  },
+  {
+    title: 'Redux',
+    url: "https://redux.js.org",
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 3,
+    points: 4,
+    objectID:0,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>My Hackers Stories</h1>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text'></input>
+
+      <hr></hr>
+      {
+        list.map(function(item){
+          return <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        })
+      }
+    </div>
+  )
+}
+
+export default App;
