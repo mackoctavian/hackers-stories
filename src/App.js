1a6f10e17e832a8ad13950c5c2ee4d9315ccac87
Mack Octavian <maestrooctavian@gmail.com>
2024-01-22 14:18:56 +0300

    Changed the map method of the List component to use arrow function

--- src/App.js
+++ src/App.js
@@ -36,14 +36,15 @@ function List() {
   return (
     <div>
       {
-        list.map(function(item){
-          return <div key={item.objectID}>
+        list.map(item => 
+          (<div key={item.objectID}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
           </div>
-        })
+          )
+        )
       }
     </div>
   )
